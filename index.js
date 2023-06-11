0d9807dc2776da4fb76fd2b68b6b4a3c19d3df4d
iam <brian.kibet2@student.moringaschool.com>
2023-06-11 19:39:38 +0000

    Completed assignment

---
+++ index.js
@@ -0,0 +1,35 @@
+// Math functions
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+// Increment and decrement functions
+function increment(n) {
+  return n + 1;
+}
+
+function decrement(n) {
+  return n - 1;
+}
+
+// makeInt function
+function makeInt(n) {
+  return parseInt(n, 10);
+}
+
+// preserveDecimal function
+function preserveDecimal(n) {
+  return parseFloat(n);
+}
